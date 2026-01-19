46c04961fadd3c8f217a132c5f3544f861394e7b
Deklin Morrissey <morrisde@oregonstate.edu>
2026-01-19 10:02:36 -0800

    Had to redo it

--- prog1.c
+++ prog1.c
@@ -1,11 +1,36 @@
+
+/*Assignment 1 - Variable Hello World
+ *Deklin Morrissey
+ *morrisde@oregonstate.edu
+ *Guyer
+ *1/18/2026
+ */
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdlib.h>
+#include <time.h> //for randomized int
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
+	srand(time(NULL)); 
+	//set random seed based on time on clock
+	int my_rand = rand() % 11; 
+	//create an int with a random value of 10
+	//Must be 11 so that 10 is within possible ints
+	if (my_rand < 5) {
+		//print the given value of my_rand and output the statement
+		printf("Output number:%d \n Eat more beef, kick less cats\n", 
+		my_rand);
+	}
+		
+		//If my_rand is between 5 and 9
+		else if (my_rand > 4 && my_rand < 10){
+			printf("Output number:%d\n FRODO LIVES\n", 
+			my_rand);
+
+		}	
+		//don't need else if statement when all scenarios are finished
+		else {
+			printf("Output number:%d\nLarn is the best Roguelike\n", 
+			my_rand);
+		}
 	
-	// TODO Complete the program
 }
